f1ed406c6d62da65aa8ea34663efde808ab64b21
Marc O'Morain <github.com@marcomorain.com>
2011-03-08 13:47:07 -0800

    Add letrec test

--- test/test.scm
+++ test/test.scm
@@ -171,4 +171,18 @@ c
 			(+ b 1))
 a
 
+(letrec ((even?
+	(lambda (n)
+		(if (zero? n)
+			#t
+			(odd? (- n 1)))))
+	(odd?
+		(lambda (n)
+			(if (zero? n)
+				#f
+				(even? (- n 1))))))
+ (even? 88))
+
 ;(let ((a 1)) )
+
+; (display ((lambda lambda lambda) 'x))
